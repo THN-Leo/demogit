e5f76a3bcf249a2999104574bb45b28d2c37be4f
Leo <namtranjr158@gmail.com>
2021-10-07 14:41:43 +0700

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package cybersoft.javacore;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+		System.out.println("Hello World Again.");
+		System.out.println("Hello World Again.");
+		System.out.printf("%s\n", "Hello World");
+		
+	}
+}
